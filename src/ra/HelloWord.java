524cda8a92abce629e80c9288e1e5206c487033a
bhnone <>
2024-09-12 22:14:14 +0900

    done

--- src/ra/HelloWord.java
+++ src/ra/HelloWord.java
@@ -3,5 +3,13 @@ package ra;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        float number = 5.7f;
+        int totalNumber = addTwoNumbers(5, 7);
+        int[] arrInt = {3, 5, 7};
+    }
+
+    public static int addTwoNumbers(int firstNumber, int secondNumber) {
+        int sumNumbers = firstNumber + secondNumber;
+        return sumNumbers;
     }
 }
