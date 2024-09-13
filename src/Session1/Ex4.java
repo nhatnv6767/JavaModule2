eefbe0f711ff7f634d4549851f28f360c1b83743
bhnone <>
2024-09-13 21:07:44 +0900

    session 1 finish

--- src/Session1/Ex4.java
+++ src/Session1/Ex4.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Ex4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float tongDiem = 0;
+        float result = 0;
         System.out.print("Nhập điểm Toán: ");
-        float diem = Float.parseFloat(scanner.nextLine());
-        tongDiem += diem;
+        float score = Float.parseFloat(scanner.nextLine());
+        result += score;
         System.out.print("Nhập điểm Lý: ");
-        diem = Float.parseFloat(scanner.nextLine());
-        tongDiem += diem;
+        score = Float.parseFloat(scanner.nextLine());
+        result += score;
         System.out.print("Nhập điểm Hoá: ");
-        diem = Float.parseFloat(scanner.nextLine());
-        tongDiem += diem;
+        score = Float.parseFloat(scanner.nextLine());
+        result += score;
 
-        System.out.printf("Trung bình cộng điểm 3 môn: %.2f", tongDiem / 3);
+        System.out.printf("Trung bình cộng điểm 3 môn: %.2f", result / 3);
 
     }
 }
--- src/Session1/Ex5.java
+++ src/Session1/Ex5.java
@@ -6,7 +6,7 @@ public class Ex5 {
     public static void main(String[] args) {
         final float PI = 3.14F;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nập vào bán kính hình tròn: ");
+        System.out.print("Nhập vào bán kính hình tròn: ");
         float r = Float.parseFloat(scanner.nextLine());
         System.out.printf("Chu vi hình tròn: %.2f - Dện tích hình tròn: %.2f", 2 * PI * r, PI * r * r);
     }
