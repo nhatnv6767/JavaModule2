1e3444687f0f7a39e738d118a4083934d6603906
bhnone <>
2024-09-16 23:18:00 +0900

    Session 2 ex2 the first step

--- src/Session2/Ex2_AnotherWay.java
+++ src/Session2/Ex2_AnotherWay.java
@@ -5,65 +5,21 @@ import java.util.Scanner;
 public class Ex2_AnotherWay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập vào một số: ");
-        long number = Long.parseLong(scanner.nextLine());
-        System.out.println(convertNumberToWords(number));
+//        System.out.print("Nhập vào một số: ");
+
+        String num = "4540204504";
+
+        // tinh so luong so 0 can them vao
+        int needZeroCount = num.length() % 3;
+        if (needZeroCount != 0) {
+            needZeroCount = 3 - needZeroCount;
+        }
+
+        // them so luong so 0 vao de % 3 == 0
+        num = "0".repeat(needZeroCount) + num;
+
+        System.out.println(num);
     }
 
-    public static String convertNumberToWords(long number) {
-        if (number == 0) {
-            return "không";
-        }
 
-        String[] units = {"", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"};
-        String[] teens = {"mười", "mười một", "mười hai", "mười ba", "mười bốn", "mười lăm", "mười sáu", "mười bảy", "mười tám", "mười chín"};
-        String[] tens = {"", "mười", "hai mươi", "ba mươi", "bốn mươi", "năm mươi", "sáu mươi", "bảy mươi", "tám mươi", "chín mươi"};
-        String[] thousans = {"", "nghìn", "triệu", "tỷ"};
-
-        String words = "";
-        int thousandIndex = 0;
-        while (number > 0) {
-            // lay 3 chu so
-            long group = number % 1000;
-            if (group > 0) {
-                words = convertGroupToWords(group, units, teens, tens) + " " + thousans[thousandIndex] + " " + words;
-            }
-            number /= 1000;
-            thousandIndex++;
-        }
-        return words.trim();
-    }
-
-    public static String convertGroupToWords(long group, String[] units, String[] teens, String[] tens) {
-        int hundred = (int) (group / 100);
-        int ten = (int) ((group % 100) / 10);
-        int unit = (int) (group % 10);
-
-        String words = "";
-        // xu ly hang tramm
-        if (hundred > 0) {
-            words += units[hundred] + " trăm ";
-            if (ten == 0 && unit == 0) {
-                words += "chẵn ";
-            } else if (unit == 0) {
-                words += "lẻ ";
-            }
-        }
-
-        if (ten > 0) {
-            if (ten == 1) {
-                words += teens[unit];
-            } else {
-                words += tens[ten];
-                if (unit > 0) {
-                    words += " " + units[unit];
-                }
-            }
-        } else if (unit > 0) {
-            words += units[unit];
-        }
-
-
-        return words.trim();
-    }
 }
