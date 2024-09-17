4f33d61726a2dd61b332b628bb0291d98c177d09
bhnone <>
2024-09-17 23:28:16 +0900

    Session 3 ex 2 done

--- src/Session3/Ex1.java
+++ src/Session3/Ex1.java
@@ -8,30 +8,30 @@ public class Ex1 {
         Scanner scanner = new Scanner(System.in);
 
 
-        int[] mang = {1, 5, 3, 7, 2, 9, 0};
-        int kichThuoc = mang.length;
+        int[] arr = {1, 5, 3, 7, 2, 9, 0};
+        int lengArr = arr.length;
 
         System.out.print("Nhập phần tử cần xóa: ");
-        int phanTuCanXoa = scanner.nextInt();
+        int deleteElement = scanner.nextInt();
 
-        int viTriXoa = -1;
-        for (int i = 0; i < kichThuoc; i++) {
-            if (mang[i] == phanTuCanXoa) {
-                viTriXoa = i;
+        int deleteIndex = -1;
+        for (int i = 0; i < lengArr; i++) {
+            if (arr[i] == deleteElement) {
+                deleteIndex = i;
                 break;
             }
         }
 
         // xoa phan tu neu tim thay
-        if (viTriXoa != -1) {
-            for (int i = viTriXoa; i < kichThuoc - 1; i++) {
-                mang[i] = mang[i + 1];
+        if (deleteIndex != -1) {
+            for (int i = deleteIndex; i < lengArr - 1; i++) {
+                arr[i] = arr[i + 1];
             }
-            kichThuoc--;
-            mang[kichThuoc] = 0;
+            lengArr--;
+            arr[lengArr] = 0;
 
-//            System.out.println("Mảng sau khi xóa: " + Arrays.toString(Arrays.copyOf(mang, kichThuoc)));
-            System.out.println("Mảng sau khi xóa: " + Arrays.toString(Arrays.copyOf(mang, kichThuoc + 1)));
+//            System.out.println("Mảng sau khi xóa: " + Arrays.toString(Arrays.copyOf(arr, lengArr)));
+            System.out.println("Mảng sau khi xóa: " + Arrays.toString(Arrays.copyOf(arr, lengArr + 1)));
         } else {
             System.out.println("Phần tử không tồn tại trong mảng.");
         }
