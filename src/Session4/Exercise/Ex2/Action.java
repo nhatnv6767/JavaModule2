2651cd5b072e8f9e42c7f9354c204b0a0aeed196
bhnone <>
2024-09-19 23:19:37 +0900

    Session 4 - Ex2 done

--- src/Session4/Exercise/Ex2/Action.java
+++ src/Session4/Exercise/Ex2/Action.java
@@ -2,6 +2,8 @@ package Session4.Exercise.Ex2;
 
 public class Action {
     public static void main(String[] args) {
-        
+        Employee employee = new Employee();
+        employee.inputData();
+        employee.displayData();
     }
 }
--- src/Session4/Exercise/Ex2/Employee.java
+++ src/Session4/Exercise/Ex2/Employee.java
@@ -8,5 +8,53 @@ package Session4.Exercise.Ex2;
 import java.util.Scanner;
 
 public class Employee {
+    private String employeeId;
+    private String employeeName;
+    private int age;
+    private String gen;
+    private double rate;
+    private double salary;
 
+    public Employee() {
+    }
+
+    public Employee(String employeeId, String employeeName, int age, String gen, double rate) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.age = age;
+        this.gen = gen;
+        this.rate = rate;
+        calSalary();
+    }
+
+    public void inputData() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập mã nhân viên: ");
+        this.employeeId = scanner.nextLine();
+        System.out.print("Nhập tên nhân viên: ");
+        this.employeeName = scanner.nextLine();
+        System.out.print("Nhập tuổi: ");
+        this.age = Integer.parseInt(scanner.nextLine());
+        System.out.print("Nhập giới tính: ");
+        this.gen = scanner.nextLine();
+        System.out.print("Nhập hệ số lương: ");
+        this.rate = Double.parseDouble(scanner.nextLine());
+
+        calSalary();
+    }
+
+    public void displayData() {
+        System.out.println("Mã nhân viên: " + employeeId);
+        System.out.println("Tên nhân viên: " + employeeName);
+        System.out.println("Tuổi: " + age);
+        System.out.println("Giới tính: " + gen);
+        System.out.println("Hệ số lương: " + rate);
+        System.out.println("Lương: " + salary);
+
+    }
+
+    public void calSalary() {
+        salary = rate * 1300000;
+    }
 }
