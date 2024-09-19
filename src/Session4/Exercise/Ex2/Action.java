28a938bf921a131d7ea6b31908b91166449e913d
bhnone <>
2024-09-19 23:05:56 +0900

    Session 4 - Ex2 init

--- src/Session4/Exercise/Ex2/Action.java
+++ src/Session4/Exercise/Ex2/Action.java
@@ -2,11 +2,6 @@ package Session4.Exercise.Ex2;
 
 public class Action {
     public static void main(String[] args) {
-        Employee myEmployee = new Employee();
-        myEmployee.inputData();
-        myEmployee.displayData();
-
-        System.out.println("Chu vi: " + myEmployee.chuVi());
-        System.out.println("Diện tích: " + myEmployee.dienTich());
+        
     }
 }
