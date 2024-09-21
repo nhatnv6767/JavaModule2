8a99c8c5d9b0a1c24ed88bec9c5e130b9cba5497
bhnone <>
2024-09-22 00:12:30 +0900

    Session 4 - Exercise Plus ex 3

--- src/ExercisePlus/Ex2/ProductImp.java
+++ src/ExercisePlus/Ex2/ProductImp.java
@@ -1,10 +1,58 @@
 package ExercisePlus.Ex2;
 
+import java.util.Scanner;
+
 public class ProductImp {
     final static int exitMenu = 10;
 
     public static void main(String[] args) {
         Product[] productArr = new Product[100];
-        int count = 0;
+        int count = 0;// so luong san pham hien co trong mang
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("*********************MENU******************");
+            System.out.println("1. Nhập thông tin n sản phẩm (n nhập từ bàn phím)");
+            System.out.println("2. Hiển thị thông tin các sản phẩm");
+            System.out.println("3. Tính lợi nhuận các sản phẩm");
+            System.out.println("4. Sắp xếp các sản phẩm theo lợi nhuận giảm dần");
+            System.out.println("5. Thống kê các sản phẩm theo giá");
+            System.out.println("6. Tìm các sản phẩm theo tên sản phẩm");
+            System.out.println("7. Nhập sản phẩm");
+            System.out.println("8. Bán sản phẩm");
+            System.out.println("9. Cập nhật trạng thái sản phẩm");
+            System.out.println("10. Thoát");
+            System.out.print("Lựa chọn của bạn: ");
+            choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    System.out.print("Nhập số lượng sản phẩm (n): ");
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    System.out.println("Thoát chương trình");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng thử lại.");
+
+            }
+
+        } while (choice != exitMenu);
     }
 }
