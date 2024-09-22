26b451be6aac060798dd917ad8956ecb076499e1
bhnone <>
2024-09-22 15:15:01 +0900

    Session 4 - Exercise in JAPAN

---
+++ src/ExercisePlus/JAPAN/Exercise_2.java
@@ -0,0 +1,16 @@
+package ExercisePlus.JAPAN;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        String text = "go with me and do something";
+
+        Pattern pattern = Pattern.compile("\\b[^g\\s]\\w*g\\w*[^g\\s]\\b");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
