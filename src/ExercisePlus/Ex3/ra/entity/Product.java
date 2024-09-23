21438147aca0f4071945879beab20da8d02ad8ee
bhnone <>
2024-09-23 21:21:55 +0900

    Session 4 - Exercise bonus ex 3

--- src/ExercisePlus/Ex3/ra/entity/Product.java
+++ src/ExercisePlus/Ex3/ra/entity/Product.java
@@ -1,4 +1,26 @@
 package ExercisePlus.Ex3.ra.entity;
 
+import java.util.Date;
+
 public class Product {
+    private String productId;
+    private String productName;
+    private float price;
+    private String description;
+    private Date created;
+    private int catalogId;
+    private int productStatus;
+
+    public Product() {
+    }
+
+    public Product(String productId, String productName, float price, String description, Date created, int catalogId, int productStatus) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+        this.created = created;
+        this.catalogId = catalogId;
+        this.productStatus = productStatus;
+    }
 }
--- src/ExercisePlus/JAPAN/Exercise_2.java
+++ src/ExercisePlus/JAPAN/Exercise_2.java
@@ -5,10 +5,14 @@ import java.util.regex.Pattern;
 
 public class Exercise_2 {
     public static void main(String[] args) {
-        String text = "present tasion";
 
-        Pattern pattern = Pattern.compile("^[p.*\\s]+$");
+        String text = "hello_world_trice";
+
+        Pattern pattern = Pattern.compile("^[a-z]+(_[a-z]+)+$");
         Matcher matcher = pattern.matcher(text);
         System.out.println(matcher.matches());
     }
 }
+
+//Pattern pattern = Pattern.compile("^[a-z]+(_[a-z]+)+$");
+//Pattern pattern = Pattern.compile("^[a-z]+(_[a-z]+)+$");
