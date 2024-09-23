cfd2185d76bf5cfa85969481740d70f986adacb0
bhnone <>
2024-09-24 01:34:07 +0900

    Session 4 - Exercise bonus ex 4 init

---
+++ src/ExercisePlus/Ex4/ra/run/StoreManagement.java
@@ -0,0 +1,22 @@
+package ExercisePlus.Ex4.ra.run;
+
+import ExercisePlus.Ex4.ra.entity.Employee;
+import ExercisePlus.Ex4.ra.entity.Order;
+import ExercisePlus.Ex4.ra.entity.Product;
+
+import java.util.Scanner;
+
+public class StoreManagement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Product[] products = new Product[100];
+        Employee[] employees = new Employee[100];
+        Order[] orders = new Order[100];
+
+        int productCount = 0, employeeCount = 0, orderCount = 0;
+
+        int choice;
+        
+    }
+}
