ffdc7efa4544d6476b40693fa82662017b825f51
bhnone <>
2024-09-24 20:54:42 +0900

    Session 5 - example explain static

---
+++ src/Session5/Example/StaticMethodDemo.java
@@ -0,0 +1,8 @@
+package Session5.Example;
+
+public class StaticMethodDemo {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println("Sinh viên 1 tính tổng 2 số 3 và 5" + student1.addTwoNumber(3, 5));
+    }
+}
