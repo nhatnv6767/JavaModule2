e4b8ee32d64b3505d039959e0cda070efe2a8e70
bhnone <>
2024-09-24 21:46:28 +0900

    Session 5 - example student management when have static value

---
+++ src/Session5/Example/studentManagement/StudentManagement.java
@@ -0,0 +1,10 @@
+package Session5.Example.studentManagement;
+
+public class StudentManagement {
+    static Student[] arrStudent = new Student[100];
+    static int currentIndex = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
