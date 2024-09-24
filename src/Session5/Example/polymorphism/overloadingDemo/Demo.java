5a08f23d8499d969088c232ed380cc78137827c8
bhnone <>
2024-09-24 21:30:21 +0900

    Session 5 - example polymorphism

---
+++ src/Session5/Example/polymorphism/overloadingDemo/Demo.java
@@ -0,0 +1,8 @@
+package Session5.Example.polymorphism.overloadingDemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        OverloadingDemo od = new OverloadingDemo();
+        od.test(12, "name");
+    }
+}
