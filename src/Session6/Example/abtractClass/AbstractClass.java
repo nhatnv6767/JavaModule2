1e08f973aa48829ca259a05201163bba4ccb01ec
bhnone <>
2024-09-26 21:06:45 +0900

    Session 6 - interface

---
+++ src/Session6/Example/abtractClass/AbstractClass.java
@@ -0,0 +1,5 @@
+package Session6.Example.abtractClass;
+
+public abstract class AbstractClass extends AbstractClassDemo {
+    public abstract int addTwoNumber(int firstNumber, int secondNumber);
+}
---
+++ src/Session6/Example/interfaceD/IDemo4.java
@@ -0,0 +1,5 @@
+package Session6.Example.interfaceD;
+
+public interface IDemo4 {
+    void test6();
+}
