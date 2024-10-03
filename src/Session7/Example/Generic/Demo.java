0be736ee1a880d846595a994554c304d454a3442
bhnone <>
2024-10-03 21:02:34 +0900

    Session 7 : interface generic

--- src/Session7/Example/Generic/Demo.java
+++ src/Session7/Example/Generic/Demo.java
@@ -11,5 +11,14 @@ public class Demo {
         GenericInheritance2<Integer> gi2 = new GenericInheritance2<>();
         GenericInheritance3<Float, Double, Integer> gi3 = new GenericInheritance3<>();
 
+        // method generic
+        MethodGeneric mg = new MethodGeneric();
+        Integer[] arrInt = {1, 3, 5, 7};
+        Float[] arrFloat = {1.2f, 5.6f, 6.6f};
+        String[] arrString = {"Mot", "Hai", "Ba"};
+        mg.printElementOfArray(arrInt);
+        mg.printElementOfArray(arrFloat);
+        mg.printElementOfArray(arrString);
+
     }
 }
