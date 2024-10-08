605c5ddb209cddc7f8316aac3a56010263dc21b6
bhnone <>
2024-10-08 22:19:34 +0900

    Session 11 (9) Optional 1

--- src/Session11/Example/ra/Optional/Demo.java
+++ src/Session11/Example/ra/Optional/Demo.java
@@ -2,11 +2,19 @@ package Session11.Example.ra.Optional;
 
 import Session11.Example.ra.Stream.Student;
 
+import java.util.Optional;
+
 public class Demo {
     public static void main(String[] args) {
         Student student = getStudent1();
-        
-        System.out.println("Ma sinh vien la: " + student.getStudentId());
+        if (student != null) {
+            System.out.println("Ma sinh vien la: " + student.getStudentId());
+        }
+
+        Optional<Student> optStudent = Optional.ofNullable(student);
+        if (optStudent.isPresent()) {
+            System.out.println("Ma sinh vien la: " + student.getStudentId());
+        }
     }
 
     public static Student getStudent1() {
