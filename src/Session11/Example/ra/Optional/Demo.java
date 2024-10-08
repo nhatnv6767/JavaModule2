fce7ce2be5d90853fff4b6d37e650ece46ceea6b
bhnone <>
2024-10-08 22:17:35 +0900

    Session 11 (9) Optional

---
+++ src/Session11/Example/ra/Optional/Demo.java
@@ -0,0 +1,15 @@
+package Session11.Example.ra.Optional;
+
+import Session11.Example.ra.Stream.Student;
+
+public class Demo {
+    public static void main(String[] args) {
+        Student student = getStudent1();
+        
+        System.out.println("Ma sinh vien la: " + student.getStudentId());
+    }
+
+    public static Student getStudent1() {
+        return null;
+    }
+}
