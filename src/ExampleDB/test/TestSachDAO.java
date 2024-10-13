fcf3f0fc2db7b42095cca68dae4a3f4339c175a0
bhnone <>
2024-10-13 21:03:44 +0900

    ExampleDB insert Data TEST

--- src/ExampleDB/test/TestSachDAO.java
+++ src/ExampleDB/test/TestSachDAO.java
@@ -5,10 +5,14 @@ import ExampleDB.model.Sach;
 
 public class TestSachDAO {
     public static void main(String[] args) {
-        Sach sach1 = new Sach("LTJAVA", "Lap trinh Java", 50000, 2025);
-        Sach sach2 = new Sach("LTC", "Lap trinh C", 70000, 2030);
+//        Sach sach1 = new Sach("LTJAVA", "Lap trinh Java", 50000, 2025);
+//        Sach sach2 = new Sach("LTC", "Lap trinh C", 70000, 2030);
+//
+//        SachDAO.getInstance().insert(sach2);
 
-        SachDAO.getInstance().insert(sach2);
-
+        for (int i = 0; i < 1000; i++) {
+            Sach sach = new Sach("LT" + i, "Lap trinh i: " + i, 50000, 2025);
+            SachDAO.getInstance().insert(sach);
+        }
     }
 }
