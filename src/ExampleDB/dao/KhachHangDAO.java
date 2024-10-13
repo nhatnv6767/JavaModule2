360b19e88986169cbfdb4f473717e49d2e468288
bhnone <>
2024-10-13 22:00:37 +0900

    ExampleDB selectAll KhachHang

--- src/ExampleDB/dao/KhachHangDAO.java
+++ src/ExampleDB/dao/KhachHangDAO.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class KhachHangDAO implements DAOInterface<KhachHang> {
 
