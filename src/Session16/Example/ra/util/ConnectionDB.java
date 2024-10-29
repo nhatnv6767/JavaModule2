6e18d196a77e8f5e5b295bc65c9c549e3e167434
bhnone <>
2024-10-29 21:09:21 +0900

    Session 16. JDBC init DB Connection

--- src/Session16/Example/ra/util/ConnectionDB.java
+++ src/Session16/Example/ra/util/ConnectionDB.java
@@ -1,5 +1,8 @@
 package Session16.Example.ra.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionDB {
     /*
     1. URL cua CSDL can ket noi den
@@ -14,4 +17,29 @@ public class ConnectionDB {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "123456";
+
+    // Phuong thuc mo ket noi va tao doi tuong Connection de lam viec voi db
+    public static Connection openConnection() {
+        // 1. Set Driver cho DriverManager
+        // 2. Khoi tao doi tuong Connection tu DriverManager
+        // 3. Tra ve doi tuong Connection
+
+        Connection conn = null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = openConnection();
+        if (conn != null) {
+            System.out.println("Ket noi thanh cong");
+        } else {
+            System.out.println("Ket noi fail");
+        }
+    }
 }
